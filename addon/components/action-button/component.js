84452709e7c9b65b08e303fe6312bf5dd1d054e0
Seth Brasile <seth.brasile@gmail.com>
2015-10-02 23:12:14 -0500

    content for action-button

--- addon/components/action-button/component.js
+++ addon/components/action-button/component.js
@@ -2,5 +2,10 @@ import Ember from 'ember';
 import layout from './template';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  tagName: 'button',
+
+  click() {
+    this.sendAction();
+  }
 });
