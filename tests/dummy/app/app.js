414dfdc4e6dc53c5c9aff20b6d593f5ea9047ad8
Seth Brasile <seth.brasile@gmail.com>
2015-10-11 13:02:15 -0500

    Added ember-suave and made adjustments to compensate for it's rules

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,14 +3,12 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var App;
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+const App = Ember.Application.extend({
+  Resolver,
   modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  podModulePrefix: config.podModulePrefix
 });
 
 loadInitializers(App, config.modulePrefix);
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
+export default Ember.Router.extend({
   location: config.locationType
 });
-
-Router.map(function() {
-});
-
-export default Router;
