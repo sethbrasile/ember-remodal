fba8abd834d35f6a6f6a5cc9326770a3c3747153
Seth Brasile <seth.brasile@gmail.com>
2016-01-13 22:47:11 -0600

    Added ember-cli-blanket

---
+++ tests/blanket-options.js
@@ -0,0 +1,18 @@
+/* globals blanket, module */
+
+const options = {
+  modulePrefix: 'ember-remodal',
+  filter: '//.*ember-remodal/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['json'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
