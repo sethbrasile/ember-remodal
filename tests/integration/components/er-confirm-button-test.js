f3e9a024f375744c93b7b9f408a5b3459d3cb9e7
Seth Brasile <seth.brasile@gmail.com>
2016-01-13 18:28:03 -0600

    custom-confirm-button

---
+++ tests/integration/components/er-confirm-button-test.js
@@ -0,0 +1,25 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('er-confirm-button', 'Integration | Component | er confirm button', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+
+  this.render(hbs`{{er-confirm-button}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:" + EOL +
+  this.render(hbs`
+    {{#er-confirm-button}}
+      template block text
+    {{/er-confirm-button}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
