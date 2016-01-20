e3884ece516ca1eef732e9029afda2fa432e37be
Seth Brasile <seth.brasile@gmail.com>
2016-01-20 00:34:29 -0600

    clean up er-button mixin

--- addon/mixins/er-button.js
+++ addon/mixins/er-button.js
@@ -17,18 +17,12 @@ export default Mixin.create({
   },
 
   _registerWithModal() {
-    const modal = this._getModal();
-    modal.set(camelize(this.get('name')), true);
-
-    this.set('modal', modal);
-    this._assertModalIsNested();
+    this.set('modal', this._getModal());
+    this.set(`modal.${camelize(this.get('name'))}`, true);
+    assert(`An "${this.get('name')}" MUST be declared inside an "ember-remodal" component block.`, !!this.get('modal'));
   },
 
   _getModal() {
     return this.nearestOfType(EmberRemodal);
-  },
-
-  _assertModalIsNested() {
-    assert(`An "${this.get('name')}" MUST be declared inside an "ember-remodal" component block.`, !!this.get('modal'));
   }
 });
