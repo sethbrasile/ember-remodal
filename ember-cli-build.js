ea43eff5940bdf5506a19296d23490d3414f71c9
Seth Brasile <seth.brasile@gmail.com>
2016-01-28 18:45:50 -0600

    re-enabled jshint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,6 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    'ember-cli-qunit': {
-      useLintTree: false
-    }
   });
 
   /*
