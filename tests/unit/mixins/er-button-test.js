69e956f885bbceeee681c518efd2c7f0a872cc3a
Seth Brasile <seth.brasile@gmail.com>
2016-02-18 18:54:51 -0600

    new tests for er-button mixin
    Since “click()” and “name” are both in the mixin now instead of
    individual buttons

--- tests/unit/mixins/er-button-test.js
+++ tests/unit/mixins/er-button-test.js
@@ -5,8 +5,17 @@ import { module, test } from 'qunit';
 module('Unit | Mixin | er button');
 
 const ErButtonObject = Ember.Object.extend(ErButtonMixin, {
-  name: 'test-name',
+  buttonType: 'test',
   _assertModalIsNested() {}
+});
+
+const ModalMock = Ember.Component.extend({
+  actionCalled: false,
+  actions: {
+    test() {
+      this.set('actionCalled', true);
+    }
+  }
 });
 
 test('it works', function(assert) {
@@ -45,21 +54,37 @@ test('"_registerWithModal" properly sets the button\'s name on the modal', funct
   mixin.set('_getModal', () => Ember.Object.create());
 
   Ember.run(() => {
-    assert.notOk(mixin.get('modal.testName'));
+    assert.notOk(mixin.get('modal.erTestButton'));
     mixin._registerWithModal();
-    assert.ok(mixin.get('modal.testName'));
+    assert.ok(mixin.get('modal.erTestButton'));
   });
 });
 
 test('"_registerWithModal" throws an error when modal is not set', function(assert) {
   let mixin = ErButtonObject.create();
   mixin.set('getModalCalled', false);
+
   mixin.set('_getModal', () => {
     mixin.set('getModalCalled', true);
     return null;
   });
 
+  Ember.run(() => assert.throws(mixin._registerWithModal));
+});
+
+test('"name" is computed based on "buttonType"', function(assert) {
+  let mixin = ErButtonObject.create();
+  Ember.run(() => assert.equal(mixin.get('name'), 'er-test-button'));
+});
+
+test('"click()" sends an action named the same as "buttonType"', function(assert) {
+  let mixin = ErButtonObject.create();
+
+  mixin.set('modal', ModalMock.create());
+
   Ember.run(() => {
-    assert.throws(mixin._registerWithModal);
+    assert.notOk(mixin.get('modal.actionCalled'));
+    mixin.click();
+    assert.ok(mixin.get('modal.actionCalled'));
   });
 });
