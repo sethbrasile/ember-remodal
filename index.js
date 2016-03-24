4ee8bc632ea3ea8e773b41d60660081c8f1211d5
Seth Brasile <seth.brasile@gmail.com>
2016-03-24 18:15:28 -0500

    ember-cli-version-checker

--- index.js
+++ index.js
@@ -11,5 +11,29 @@ module.exports = {
     app.import(app.bowerDirectory + '/remodal/dist/remodal.css');
     app.import(app.bowerDirectory + '/remodal/dist/remodal-default-theme.css');
     app.import('vendor/style.css');
+  },
+
+  hasContextualComponents: function() {
+    var VersionChecker = require('ember-cli-version-checker');
+
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember', 'bower');
+
+    var isBetaOrCanary = ['beta', 'canary'].filter(function(version) {
+      return dep.version.indexOf(version) >= 0;
+    });
+
+    return !!(dep.satisfies('>= 2.3.0 < 3.0.0') || isBetaOrCanary.length > 0);
+  },
+
+  treeForAddonTemplates: function(tree) {
+    var path = require('path');
+    var baseTemplatesPath = path.join(this.root, 'addon/templates');
+
+    if (this.hasContextualComponents()) {
+      return this.treeGenerator(path.join(baseTemplatesPath, 'greater-than-2.3'));
+    } else {
+      return this.treeGenerator(path.join(baseTemplatesPath, 'less-than-2.3'));
+    }
   }
 };
