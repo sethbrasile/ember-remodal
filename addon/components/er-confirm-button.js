5322aecd2743131b440013a7acdea2011ccae7a9
Seth Brasile <seth.brasile@gmail.com>
2016-03-27 16:33:39 -0500

    don't need layouts any more for cancel and confirm

--- addon/components/er-confirm-button.js
+++ addon/components/er-confirm-button.js
@@ -1,9 +1,6 @@
 import Ember from 'ember';
-import layout from '../templates/components/er-confirm-button';
 import ButtonMixin from '../mixins/er-button';
 
 const { Component } = Ember;
 
-export default Component.extend(ButtonMixin, {
-  layout
-});
+export default Component.extend(ButtonMixin);
