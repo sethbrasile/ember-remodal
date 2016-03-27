d497656a9d7caf0d94fff5e8e0b88cb8b0169736
Seth Brasile <seth.brasile@gmail.com>
2016-03-27 14:33:39 -0500

    deprecate er-x-button components

--- addon/mixins/er-button.js
+++ addon/mixins/er-button.js
@@ -3,6 +3,7 @@ import EmberRemodal from '../components/ember-remodal';
 
 const {
   assert,
+  deprecate,
   String: { camelize },
   run: { scheduleOnce },
   Mixin
@@ -11,6 +12,7 @@ const {
 export default Mixin.create({
   tagName: 'span',
   modal: null,
+  deprecateComponent: true,
 
   didRender() {
     scheduleOnce('afterRender', this, '_registerWithModal');
@@ -20,6 +22,12 @@ export default Mixin.create({
     this.set('modal', this._getModal());
     this.set(`modal.${camelize(this.get('name'))}`, true);
     assert(`An "${this.get('name')}" MUST be declared inside an "ember-remodal" component block.`, !!this.get('modal'));
+
+    deprecate(
+      `ember-remodal's ${this.get('name')} is deprecated and is removed in 1.0.0. When you are able to upgrade Ember to 2.3 or greater, please use contextual components instead. See http://sethbrasile.github.io/ember-remodal/#/components`,
+      !this.get('deprecateComponent'),
+      { id: `ember-remodal.${this.get('name')}`, until: '1.0.0' }
+    );
   },
 
   _getModal() {
