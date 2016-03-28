4139de8d05226b8b2aee8f50beb791a1d2b8c85f
Seth Brasile <seth.brasile@gmail.com>
2016-03-27 23:40:25 -0500

    mobile nav works in dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,7 @@ module.exports = function(defaults) {
   */
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
   return app.toTree();
 };
