001e1361530cfa53191bce1357c8cd3b2f93aa09
Seth Brasile <seth.brasile@gmail.com>
2016-03-27 20:19:13 -0500

    cleanup

--- tests/unit/components/er-button-test.js
+++ tests/unit/components/er-button-test.js
@@ -1,6 +1,10 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import Ember from 'ember';
 
+const {
+  run
+} = Ember;
+
 moduleForComponent('er-button', 'Unit | Component | er button', {
   // Specify the other units that are required for this test
   needs: ['component:ember-wormhole'],
@@ -26,8 +30,8 @@ test('"destination" is undefined when modalId is not set', function(assert) {
 test('"destination" exists when modalId is set', function(assert) {
   let component = this.subject();
 
-  Ember.run(() => this.$().append('<div id="open-button-test"></div>'));
-  Ember.run(() => component.set('modalId', 'test'));
+  run(() => this.$().append('<div id="open-button-test"></div>'));
+  run(() => component.set('modalId', 'test'));
 
   this.render();
 
@@ -37,8 +41,8 @@ test('"destination" exists when modalId is set', function(assert) {
 test('"destination" is correct when modalId is set', function(assert) {
   let component = this.subject();
 
-  Ember.run(() => this.$().append('<div id="open-button-test"></div>'));
-  Ember.run(() => component.set('modalId', 'test'));
+  run(() => this.$().append('<div id="open-button-test"></div>'));
+  run(() => component.set('modalId', 'test'));
 
   this.render();
 
