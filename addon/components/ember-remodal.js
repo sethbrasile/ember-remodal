b653c3bbd3aa110cad2248638a5ab602d3858841
Seth Brasile <seth.brasile@gmail.com>
2016-04-14 12:08:08 -0500

    swapped didInitAttrs to didInsertElement
    didInitAttrs is being deprecated. Also the fast boot team is suggesting
    anything that touches jQuery to be placed in didInsertElement for best
    fasboot compatibility.

--- addon/components/ember-remodal.js
+++ addon/components/ember-remodal.js
@@ -37,7 +37,7 @@ export default Component.extend({
   erCancelButton: false,
   erConfirmButton: false,
 
-  didInitAttrs() {
+  didInsertElement() {
     let opts = this.get('options');
 
     if (opts) {
