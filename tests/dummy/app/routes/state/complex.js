09fb6855bafd787a6661326a7eaed9ea19ccb371
Seth Brasile <seth.brasile@gmail.com>
2016-06-03 13:51:55 -0500

    "complex" state example

--- tests/dummy/app/routes/state/complex.js
+++ tests/dummy/app/routes/state/complex.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.A([
+      Ember.Object.create({
+        owner: { name: 'Seth' },
+        name: 'Moose',
+        breed: 'English Mastiff',
+        sex: 'male',
+        size: 'retardedly big'
+      }),
+      Ember.Object.create({
+        owner: { name: `Seth's Aunt` },
+        name: 'Buttons',
+        breed: 'Pomeranian',
+        sex: 'female',
+        size: 'stupid'
+      }),
+      Ember.Object.create({
+        owner: { name: 'Matt' },
+        name: 'Bandit',
+        breed: 'of indeterminate breed',
+        sex: 'male',
+        size: 'reasonably'
+      })
+    ]);
+  }
 });
