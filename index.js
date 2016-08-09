9a6ab1e4032f091273f2e7fae7f6df8fc0e3f5dd
Francesco Novy <francesco@cropster.com>
2016-08-09 08:48:18 +0200

    Fix import in engines

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
+    // In nested addons, app.bowerDirectory might not be available
+    var bowerDirectory = app.bowerDirectory || 'bower_components';
+    // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
+    var importShim = typeof this.import !== 'undefined' ? this : app;
+
     if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(app.bowerDirectory + '/remodal/dist/remodal.js');
+      importShim.import(bowerDirectory + '/remodal/dist/remodal.js');
     }
 
-    app.import(app.bowerDirectory + '/remodal/dist/remodal.css');
-    app.import(app.bowerDirectory + '/remodal/dist/remodal-default-theme.css');
-    app.import('vendor/style.css');
+    importShim.import(bowerDirectory + '/remodal/dist/remodal.css');
+    importShim.import(bowerDirectory + '/remodal/dist/remodal-default-theme.css');
+    importShim.import('vendor/style.css');
   }
 };
