eaf8331a8cc66b9e76530e34f7da7b20d29d3447
Jake LeBoeuf <dev@jakeleboeuf.com>
2017-02-11 19:38:49 -0500

    removes bower dependency injection

--- blueprints/ember-remodal/index.js
+++
@@ -1,8 +0,0 @@
-/*jshint node:true*/
-module.exports = {
-  normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    return this.addBowerPackageToProject('remodal', '1.1.0');
-  }
-};
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 /* jshint node: true */
 'use strict';
+var path = require("path");
+var resolve = require("resolve");
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-remodal',
 
+  treeForVendor(tree) {
+    var remodalPath = path.dirname(resolve.sync('remodal'));
+    var remodalTree = new Funnel(remodalPath, {
+      files: [
+        'remodal.js',
+        'remodal.css',
+        'remodal-default-theme.css'
+      ],
+      destDir: '/remodal/dist',
+    });
+
+    return new MergeTrees([tree, remodalTree]);
+  },
+
   included: function(app) {
     this._super.included(app);
-
-    // In nested addons, app.bowerDirectory might not be available
-    var bowerDirectory = app.bowerDirectory || 'bower_components';
 
     // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
     var importShim = typeof this.import !== 'undefined' ? this : app;
 
     if (!process.env.EMBER_CLI_FASTBOOT) {
-      importShim.import(bowerDirectory + '/remodal/dist/remodal.js');
+      importShim.import('vendor/remodal/dist/remodal.js');
     }
 
-    importShim.import(bowerDirectory + '/remodal/dist/remodal.css');
-    importShim.import(bowerDirectory + '/remodal/dist/remodal-default-theme.css');
+    importShim.import('vendor/remodal/dist/remodal.css');
+    importShim.import('vendor/remodal/dist/remodal-default-theme.css');
     importShim.import('vendor/style.css');
   }
 };
