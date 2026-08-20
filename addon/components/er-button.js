9745a297ac31c37d108f9ef6ceabd77db569f76b
Seth Brasile <seth.brasile@gmail.com>
2026-08-20 10:13:51 -0500

    feat: 2.19 runtime deprecations on the remodal service and er-button

--- addon/components/er-button.js
+++ addon/components/er-button.js
@@ -1,8 +1,10 @@
 /* eslint-disable ember/closure-actions */
 
 import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 import Component from '@ember/component';
 import layout from '../templates/components/er-button';
+import { warnOnce } from '../utils/deprecations';
 
 export default Component.extend({
   layout,
@@ -20,5 +22,18 @@ export default Component.extend({
         return `open-button-${modalId}`;
       }
     }
-  })
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    if (this.get('_yielded') !== true) {
+      warnOnce(
+        getOwner(this).resolveRegistration('config:environment'),
+        'ember-remodal.er-button-direct',
+        'Invoking ember-remodal/er-button directly is broken by 3.0: the import path moved, modalId= became @destination (an Element, not an id), and action= became @onClick. Prefer the yielded m.open / m.confirm / m.cancel, which bind both for you.',
+        'er-button-the-import-path-moved-and-both-arguments-were-renamed'
+      );
+    }
+  }
 });
--- addon/services/remodal.js
+++ addon/services/remodal.js
@@ -1,23 +1,41 @@
 import { assert } from '@ember/debug';
-import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 import Service from '@ember/service';
+import { warnOnce, INTERNAL_CALL } from '../utils/deprecations';
+
+function deprecatedOptionAlias(optionName) {
+  let dependentKey = `ember-remodal.${optionName}`;
+
+  return computed(dependentKey, {
+    get() {
+      this._warnOptionAlias(optionName);
+      return this.get(dependentKey);
+    },
+    set(key, value) {
+      this._warnOptionAlias(optionName);
+      this.set(dependentKey, value);
+      return value;
+    }
+  });
+}
 
 export default Service.extend({
   modal: null,
-  title: alias('ember-remodal.title'),
-  text: alias('ember-remodal.text'),
-  confirmButton: alias('ember-remodal.confirmButton'),
-  cancelButton: alias('ember-remodal.cancelButton'),
-  disableNativeClose: alias('ember-remodal.disableNativeClose'),
-  disableForeground: alias('ember-remodal.disableForeground'),
-  disableAnimation: alias('ember-remodal.disableAnimation'),
-  buttonClasses: alias('ember-remodal.buttonClasses'),
-  modifier: alias('ember-remodal.modifier'),
-  closeOnEscape: alias('ember-remodal.closeOnEscape'),
-  closeOnCancel: alias('ember-remodal.closeOnCancel'),
-  closeOnConfirm: alias('ember-remodal.closeOnConfirm'),
-  hashTracking: alias('ember-remodal.hashTracking'),
-  closeOnOutsideClick: alias('ember-remodal.closeOnOutsideClick'),
+  title: deprecatedOptionAlias('title'),
+  text: deprecatedOptionAlias('text'),
+  confirmButton: deprecatedOptionAlias('confirmButton'),
+  cancelButton: deprecatedOptionAlias('cancelButton'),
+  disableNativeClose: deprecatedOptionAlias('disableNativeClose'),
+  disableForeground: deprecatedOptionAlias('disableForeground'),
+  disableAnimation: deprecatedOptionAlias('disableAnimation'),
+  buttonClasses: deprecatedOptionAlias('buttonClasses'),
+  modifier: deprecatedOptionAlias('modifier'),
+  closeOnEscape: deprecatedOptionAlias('closeOnEscape'),
+  closeOnCancel: deprecatedOptionAlias('closeOnCancel'),
+  closeOnConfirm: deprecatedOptionAlias('closeOnConfirm'),
+  hashTracking: deprecatedOptionAlias('hashTracking'),
+  closeOnOutsideClick: deprecatedOptionAlias('closeOnOutsideClick'),
 
   open(name = 'ember-remodal', opts = null) {
     let modal = this.get(name);
@@ -27,7 +45,7 @@ export default Service.extend({
         modal.setProperties(opts);
       }
 
-      return modal.open();
+      return modal.open(INTERNAL_CALL);
     } else {
       this._modalNotSetError(name);
     }
@@ -37,13 +55,32 @@ export default Service.extend({
     let modal = this.get(name);
 
     if (modal) {
-      return modal.close();
+      return modal.close(INTERNAL_CALL);
     } else {
       this._modalNotSetError(name);
     }
   },
 
+  _warnOptionAlias(optionName) {
+    warnOnce(
+      this._getConfig(),
+      'ember-remodal.service-option-aliases',
+      `The service's option alias properties are removed in 3.0 (first seen: "${optionName}"). Pass options through service.open(name, options) instead.`,
+      'the-services-property-aliases-are-removed'
+    );
+  },
+
+  _getConfig() {
+    return getOwner(this).resolveRegistration('config:environment');
+  },
+
   _modalNotSetError(name) {
+    warnOnce(
+      this._getConfig(),
+      'ember-remodal.open-on-unrendered-name',
+      `open()/close() was called for "${name}", which is not currently rendered. 3.0 returns a rejected promise in every build (2.x throws in development and silently no-ops in production).`,
+      'serviceopen--close-on-an-unrendered-name-always-reject'
+    );
     assert(
       `The requested modal, "${name}" can not be opened because it is not rendered in the current route. In order to use ember-remodal as a service, an instance of {{ember-remodal}} must currently be rendered, with "forService=true". Try putting it in your application template.`
     );
